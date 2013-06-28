9b519d18162ff6f27426208f76e39bd1a28132db
menxu <menxu_work@163.com>
2013-06-28 11:59:49 +0800

    socket java

---
+++ client1.rb
@@ -0,0 +1,21 @@
+require 'socket'
+require 'json'
+
+class Client1
+  # java -classpath jar名字.jar 包名.类名 
+
+  s = TCPSocket.new '127.0.0.1', 10001
+
+
+  method = 'public void run(){ System.out.println("how are you"); }'
+  scess = ' new Hello().run();'
+  h = { :method => method , :scess => scess }
+  s.send(h.to_json.to_str + "\n", 0)
+  s.flush
+
+  while line = s.gets # Read lines from socket
+    puts line         # and print them
+  end
+  s.close  
+
+end
---
+++ socket/temp/Hello.java
@@ -0,0 +1,7 @@
+package temp.com; 
+ public class  Hello{
+public void run(){ System.out.println("how are you"); }
+ public static void main (String[] args){
+ new Hello().run();
+ }
+ }
